61355832af3cb05bdbe81c2ea15b3d0a9ca72955
newFaizanAli <fa7711598@gmail.com>
2026-04-07 17:11:50 +0500

    Update Changes

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,9 +1,6 @@
 import ProtectedLayout from "./ProtectedLayout";
 import MainLayout from "./MainLayout";
-import RoleProtected from "./RoleProtected";
-
 export {
     ProtectedLayout,
     MainLayout,
-    RoleProtected
 }
